5278c47f07b2820179e46086c3dc7842e64bc26b
isaac-meyer <118081634+isaac-meyer@users.noreply.github.com>
2022-11-12 15:13:28 -0700

    set up repository

---
+++ churn-analysis.py
@@ -0,0 +1,11 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+import warnings
+warnings.filterwarnings('ignore')
